45ce5c0b4f568e72e2075074df24eec9aae59341
Artem Ilyumzhinov <ilumzhinov@gmail.com>
2022-11-28 15:00:12 +0300

    Initial Commit

---
+++ flickrClient/flickrClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  flickrClientApp.swift
+//  flickrClient
+//
+//  Created by Artem Ilyumzhinov on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct flickrClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
